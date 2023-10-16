76cfcb4d2a7da267c4b8e720e3673c7aa251e8f1
shazia-963 <119720277+shazia-963@users.noreply.github.com>
2023-10-16 23:56:52 +0530

    firstcommit
    main

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom";
+// import { ReactDOM } from "react-router-dom";
+
+import App from './App1.js';
+// import "./index.css";
+ReactDOM.render(
+
+  <App/>
+
+  , document.getElementById("root"))
+
+
+
